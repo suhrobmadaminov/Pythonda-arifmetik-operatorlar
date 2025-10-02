af6d04d5cc751d0a681c73f3726d7c10248ef88e
Suhrob Madaminov <suhrobmadaminov12@gmail.com>
2025-10-02 22:08:23 +0500

    Add files via upload

---
+++ suhrob masala.py
@@ -0,0 +1,204 @@
+# # 1
+#
+# a=int(input("Birinchi sonni kiriting:"))
+# b=int(input("Ikkinchi sonni kiriting:"))
+#
+# print(F"yigindi",a+b)
+
+
+# 2
+#
+# l=int(input("Uzunlikni kiriting"))
+# e=int(input("Enini kiriting :"))
+#
+# yuza = l * e
+# print(f"YUZI:",yuza)
+
+# 3
+
+#
+# x=int(input("1-sonni kiriting:"))
+# y=int(input("2-sonni kiriting:"))
+# z=int(input("3-sonni kiriting:"))
+#
+# ortacha = (x+y+z)/3
+# print(f"ORTACHA Qiymat",ortacha)
+
+# 4
+
+# a=int(input("A sonni kiritinf :"))
+# b=int(input("B sonni kiritinf :"))
+#
+# peremetr =2*(a+b)
+#
+# print(f"Peremetr",peremetr)
+
+
+# 5
+
+# h=int(input("Soatni kiriting:"))
+# m=int(input("Minutni kiriting:"))
+#
+# jami_daqiqa=h*60+m
+#
+# print(f"JAMI daqiqa:",jami_daqiqa)
+#
+# # 6
+#
+# usd=int(input("Dollar miqdorini kiriting:$"))
+#
+# sum=usd*12500
+#
+# print(f"JAMI pul",sum)
+
+
+# 7
+
+# a=int(input("A sonni kiriting:"))
+# b=int(input("B sonini kiriting:"))
+#
+# farq=abs(a-b)
+#
+# print(f"FARQI:",farq)
+#
+
+
+# 8
+
+# x=int(input("ASOSNI KIRITING:"))
+# n=int(input("DARAJANI KIRITING:"))
+#
+# power=x**n
+#
+# print(power)
+
+# 9
+
+# a=int(input("1-sonni kiriting;"))
+# b=int(input("2-sonni kiriting:"))
+#
+# qoldiq=a//b
+#
+# print(f"Qoldiq:",qoldiq)
+
+
+# 10
+
+# a=5
+# b=10
+#
+# a,b =b,a
+# print(a)
+# print(b)
+#
+
+
+
+
+#HAYOTIY MUAMMOLAR
+
+
+# 1
+
+# total_rent=10000000
+# n=13
+#
+# per_month=total_rent/n
+#
+# print(f"Har oyda:",per_month)
+
+
+
+# 2
+
+
+
+# d=12000
+# v=60
+#
+# time=d/v
+# print(f"Yolni bosib otish uchun:",time)
+
+
+
+# 3
+
+# seats=int(input("Seatlar sonini kiriting:"))
+# passengers=int(input("Passengers sonini kiriting:"))
+
+# per_person=passengers//seats
+# left=passengers%seats
+
+# print(f"har biriga teng chiptalar miqdori:",per_person)
+# print(f"Qolgani:",left)
+
+
+# 4
+
+# consumption=int(input("Consumptionni kiriting:"))
+# d=int(input("bosilgan masofani kiriting:"))
+
+# fuel=consumption*d/100
+
+# print(f"sarf qilingan yoqilgi miqdori:",fuel)
+
+
+# 5
+
+# salary=int(input("Salaryni kiriting:"))
+# net=salary*(1-0.12)
+
+# print(f"netto maosh:",net)
+
+
+# 6
+
+# amount=int(input("Amountni kiriting:"))
+# days=int(input("Kechikish kunini kiriting:"))
+
+# penalty=amount*0.001*days
+# total=amount+penalty
+
+# print(f"Jami tolov:",total)
+
+
+# 7
+
+# price=int(input("Mahsulot narxi:"))
+# discount=int(input("Chegirma:"))
+
+# paid=price*(1-discount/100)
+
+# print(f"Tolanadigan summa:",paid)
+
+
+# 8
+
+# P=int(input("Asosni kiriting:"))
+# r=int(input("Foiz stavkasini kiriting:"))
+# t=int(input("Yillarini kiriting:"))
+
+# interest=P*r/100*t
+# total=P+interest
+
+# print(f"Jami summa:",total)
+
+
+# 9
+
+# base_qty=int(input("Retsept grammini kiriting:"))
+# people=int(input("Kishi sonini kiriting:"))
+
+# per_person=base_qty/4
+# needed=per_person*people
+
+# print(f"Shuncha gramm kerak:",needed)
+
+
+# 10
+
+# bill=int(input("Hisob kitobni kiriting:"))
+# tip=bill*0.10
+# total=bill+tip
+
+# print(f"Jami summa:",total)
